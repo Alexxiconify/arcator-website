71fbc62d859a78a70b999de9e76af29403ecbe3a
Alexxiconify <taylorallred04@gmail.com>
2025-12-28 16:46:28 -0700

    alpha-cleanup-3.0

--- email-integration.js
+++
@@ -1,9 +0,0 @@
-// Stub implementations for email integration - replace with actual logic when needed
-export const getEmailJSStatus = () => ({serviceId: '', templateId: '', publicKey: ''});
-export const getSMTPServerStatus = () => ({host: '', port: '', secure: false, username: '', password: ''});
-export const initializeEmailJS = () => {};
-export const initializeSMTPIntegration = () => {};
-export const saveCredentials = (type, credentials) => console.log('saveCredentials:', type, credentials);
-export const sendEmailViaSMTP = (to, subject) => Promise.resolve(console.log('sendEmail:', to, subject));
-export const testEmailJSConnection = () => Promise.resolve(true);
-export const testSMTPServerConnection = () => Promise.resolve(true);
