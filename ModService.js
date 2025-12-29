7328e7b9851a98c2fd56885a6fdbda689a297714
Alexxiconify <taylorallred04@gmail.com>
2025-12-29 11:34:58 -0700

    alpha-cleanup-3.2.17

--- ModService.js
+++ ModService.js
@@ -1,4 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.esm.min.js';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm';
 import AuthService from './AuthService.js';
 import DataService from './DataService.js';
